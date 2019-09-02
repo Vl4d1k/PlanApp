3a568dc3e607e1403fee610f3d66ff8730565615
vlad1 <vlad1k9369@gmail.com>
2019-09-02 21:29:57 +0300

    Добавление класса Note

---
+++ PlanApp/Note.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanApp
+{
+    class Note
+    {
+        int number;
+        string description;
+        DateTime startDate;
+        DateTime endDate;
+
+        public Note(int _number, string _description, DateTime _startDate, DateTime _endDate)
+        {
+            number = _number;
+            description = _description;
+            startDate = _startDate;
+            endDate = _endDate;
+        }
+
+    }
+}
